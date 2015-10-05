add7f8169697b8a947d9dc17530c34c9ca7b5e74
Ian Mrak <ianmrak@gmail.com>
2015-10-04 19:57:58 -0500

    stuff

---
+++ js/controllers/illustrationController.js
@@ -0,0 +1,10 @@
+app.controller('illustrationController', ['$scope', function($scope) {
+  $scope.items = [ {
+    title: "this cool thing",
+    link: "https://...",
+    description: "..."
+  },];
+  $scope.lightboxFunction = function (e) {
+
+  }
+}])
