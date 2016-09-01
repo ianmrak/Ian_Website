8c601db3802c919949cc1587989621f123a6e3f5
Ian Mrak <ianmrak@gmail.com>
2016-08-31 18:02:59 -0700

    [sass] Gulp-sass now compiling correctly.

--- Gulpfile.js
+++ Gulpfile.js
@@ -4,12 +4,11 @@ const watch = require('gulp-watch');
 const concat = require('gulp-concat');
 
 gulp.task('styles', function() {
-  return gulp.src('./public/**/*.scss')
+  return gulp.src('./public/sass/styles.scss')
   .pipe(sass().on('error', sass.logError))
-  .pipe(concat('styles.css'))
   .pipe(gulp.dest('./public/css/'))
 });
 
 gulp.task('watch', function() {
-  gulp.watch('public/**/*.scss', ['styles']);
+  gulp.watch('public/sass/**/*.scss', ['styles']);
 });
